ede675c62ea160ba1ec74b7f9e8699fafd36e095
roniabusayeed <roni.abusayeed@gmail.com>
2020-12-08 21:56:51 +0600

    Output a list of all cities where no street connects it to another city in another country

--- main.c
+++ main.c
@@ -6,6 +6,11 @@
 #include "street.h"
 
 
+// Prototypes.
+area_t find_area_struct(int id, int area_list_size, area_t* area_list);
+int find_country_id(area_t area , int area_list_size, area_t* area_list);
+
+
 int main(int argc, char** argv)
 {
 	// Ensure proper usage.
@@ -57,9 +62,60 @@ int main(int argc, char** argv)
 	street_t* street_list = NULL;
 	size_t street_list_size;
 	load_streets(&street_list, &street_list_size, street_file);
-
 	
 	
+	for(int i = 0; i < city_list_size; i++)
+	{	
+		city_t current_city = city_list[i];
+		int current_city_area_id = current_city.areaID;
+		area_t current_city_area = find_area_struct(current_city_area_id, area_list_size, area_list);
+		int current_country_id = find_country_id(current_city_area, area_list_size, area_list);
+		
+		int current_city_connected[256];
+		int current_city_connected_size = 0;
+		
+		for(int streetIt = 0; streetIt < street_list_size; streetIt++)
+		{
+			street_t current_street = street_list[streetIt];
+			if(current_street.fromCityID == current_city.cityID)
+			{
+				current_city_connected[current_city_connected_size] = current_street.toCityID;
+				current_city_connected_size++;
+			}else if(current_street.toCityID == current_city.cityID)
+			{
+				current_city_connected[current_city_connected_size] = current_street.fromCityID;
+				current_city_connected_size++;
+			}	
+		}
+		
+		int flag = 0;
+		
+		for(int j = 0; j < city_list_size; j++)
+		{
+			if(i != j)
+			{
+				city_t next_city = city_list[j];
+				int next_city_area_id = next_city.areaID;
+				area_t next_city_area = find_area_struct(next_city_area_id, area_list_size, area_list);
+				int next_country_id = find_country_id(next_city_area, area_list_size, area_list);
+				
+				if(current_country_id != next_country_id)
+				{
+					for(int current_streetIt = 0; current_streetIt < current_city_connected_size; current_streetIt++){
+						if(current_city_connected[current_streetIt] == next_city.cityID)
+						{
+							flag = 1;
+							break;
+						}
+					}
+				}
+			}
+		}
+		
+		if(flag == 0){
+			printf("%s\n", current_city.name);
+		}
+	}
 	
 	// Free memory.
 	free(city_list);
@@ -72,5 +128,32 @@ int main(int argc, char** argv)
 	fclose(street_file);
 
 	// Success.
+	return 0;
+}
+
+
+area_t find_area_struct(int id, int area_list_size, area_t* area_list)
+{
+	for(int areaIt = 0; areaIt < area_list_size; areaIt ++)
+	{
+		if(area_list[areaIt].areaID == id)
+		{
+			return area_list[areaIt];
+		}
+	}
+}
+
+
+int find_country_id(area_t area , int area_list_size, area_t* area_list)
+{
+	if (area.type == 'L')
+	{
+		return area.areaID;
+	}
+	else if(area.type == 'R')
+	{
+		area_t temp = find_area_struct(area.IDupperArea, area_list_size, area_list);
+		return temp.areaID;
+	}
 	return 0;
 }
